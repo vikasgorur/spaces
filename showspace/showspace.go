65ff29caabdcbf8d673c2cdd496d11174a990639
Vikas Gorur <vikasgp@gmail.com>
2016-02-23 15:25:34 +0530

    Add some tests

--- showspace/showspace.go
+++ showspace/showspace.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-  "bufio"
-  "regexp"
-  "fmt"
-  "os"
-  "github.com/fatih/color"
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/fatih/color"
 )
 
 var trailingSpace = regexp.MustCompile("\\s+$")
@@ -13,14 +14,18 @@ var redHighlight = color.New(color.BgRed).SprintfFunc()
 var highlightedSpace = redHighlight(" ")
 
 // replace trailing spaces with a red highlight
+func replaceTrailingSpaces(s string, r string) string {
+	return trailingSpace.ReplaceAllString(s, r)
+}
+
 func highlightTrailingSpaces(s string) string {
-  return trailingSpace.ReplaceAllString(s, highlightedSpace)    
+	return replaceTrailingSpaces(s, highlightedSpace)
 }
 
 func main() {
-  input := bufio.NewScanner(os.Stdin)
-  
-  for input.Scan() {
-    fmt.Println(highlightTrailingSpaces(input.Text()))
-  }  
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		fmt.Println(replaceTrailingSpaces(input.Text(), highlightedSpace))
+	}
 }
