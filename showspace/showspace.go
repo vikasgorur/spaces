77eedbacee35148b6f4d7240d47e2fb1cce3da16
Vikas Gorur <vikasgp@gmail.com>
2016-02-25 20:16:44 +0530

    Use command line arguments

--- showspace/showspace.go
+++ showspace/showspace.go
@@ -17,11 +17,29 @@ func trimTrailing(s string) (string, string) {
 	return nonspace, s[len(nonspace):len(s)]
 }
 
-func main() {
-	input := bufio.NewScanner(os.Stdin)
+func showSpaces(f *os.File) {
+	input := bufio.NewScanner(f)
 
 	for input.Scan() {
 		nonspace, spaces := trimTrailing(input.Text())
 		fmt.Printf("%s%s\n", nonspace, redHighlight(spaces))
+	}
+}
+
+func main() {
+	files := os.Args[1:]
+	if len(files) == 0 {
+		showSpaces(os.Stdin)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "showspace: %v\n", err)
+				continue
+			}
+
+			showSpaces(f)
+			f.Close()
+		}
 	}
 }
