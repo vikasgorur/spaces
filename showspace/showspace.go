8a8c6978a979797e9e5579b7d1d220b0e3fadd68
Vikas Gorur <vikasgp@gmail.com>
2016-02-25 19:57:21 +0530

    Use unicode definition of spaces

--- showspace/showspace.go
+++ showspace/showspace.go
@@ -4,28 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"regexp"
+	"strings"
+	"unicode"
 
 	"github.com/fatih/color"
 )
 
-var trailingSpace = regexp.MustCompile("\\s+$")
 var redHighlight = color.New(color.BgRed).SprintfFunc()
-var highlightedSpace = redHighlight(" ")
 
-// replace trailing spaces with a red highlight
-func replaceTrailingSpaces(s string, r string) string {
-	return trailingSpace.ReplaceAllString(s, r)
-}
-
-func highlightTrailingSpaces(s string) string {
-	return replaceTrailingSpaces(s, highlightedSpace)
+func trimTrailing(s string) (string, string) {
+	nonspace := strings.TrimRightFunc(s, unicode.IsSpace)
+	return nonspace, s[len(nonspace):len(s)]
 }
 
 func main() {
 	input := bufio.NewScanner(os.Stdin)
 
 	for input.Scan() {
-		fmt.Println(replaceTrailingSpaces(input.Text(), highlightedSpace))
+		nonspace, spaces := trimTrailing(input.Text())
+		fmt.Printf("%s%s\n", nonspace, redHighlight(spaces))
 	}
 }
