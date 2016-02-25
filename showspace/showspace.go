432e453889bd8492e4e3881d6fd52739e62ce762
Vikas Gorur <vikasgp@gmail.com>
2016-02-25 20:42:48 +0530

    Add a check mode

--- showspace/showspace.go
+++ showspace/showspace.go
@@ -13,11 +13,13 @@ import (
 
 var redHighlight = color.New(color.BgRed).SprintfFunc()
 
+// separate the string and its trailing spaces and return them
 func trimTrailing(s string) (string, string) {
 	nonspace := strings.TrimRightFunc(s, unicode.IsSpace)
 	return nonspace, s[len(nonspace):len(s)]
 }
 
+// highlight trailing spaces in the file
 func showSpaces(f *os.File) {
 	input := bufio.NewScanner(f)
 
@@ -27,20 +29,39 @@ func showSpaces(f *os.File) {
 	}
 }
 
+// return true if any line in the file has trailing spaces
+func hasSpaces(f *os.File) {
+	input := bufio.NewScanner(f)
+
+	for input.Scan() {
+		_, spaces := trimTrailing(input.Text())
+		if spaces != "" {
+			os.Exit(1)
+		}
+	}
+}
+
 func main() {
 	var help = flag.Bool("h", false, "show usage")
+	var check = flag.Bool("c", false, "check mode; exit 0 if there are no trailing spaces, nonzero otherwise")
 
 	flag.Parse()
 	if *help {
-		fmt.Println("Usage:")
-		fmt.Println("    showspace [file1 ...]")
-		fmt.Println("    cat <file> | showspace")
+		flag.Usage()
 		os.Exit(1)
+	}
+
+	var mode func(*os.File)
+
+	if *check {
+		mode = hasSpaces
+	} else {
+		mode = showSpaces
 	}
 
 	files := flag.Args()
 	if len(files) == 0 {
-		showSpaces(os.Stdin)
+		mode(os.Stdin)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -49,8 +70,10 @@ func main() {
 				continue
 			}
 
-			showSpaces(f)
+			mode(f)
 			f.Close()
 		}
 	}
+
+	os.Exit(0)
 }
